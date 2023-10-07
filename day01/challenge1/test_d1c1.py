c93c634465a832a5326d45a3fbd7def7c478a258
Avat <avatrezaei88@gmail.com>
2023-10-08 01:15:36 +0330

    Completed Day 2 Challenge 1+2

--- day01/challenge1/test_d1c1.py
+++ day01/challenge1/test_d1c1.py
@@ -7,7 +7,7 @@ from d1c1 import count_increases
 
 def test_count_increases_from_file():
     current_directory = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(current_directory, "input.txt")
+    file_path = os.path.join(current_directory, "../input.txt")
     with open(file_path, "r") as file:
         depths = [int(line.strip()) for line in file.readlines()]
     result = count_increases(depths) 
--- day01/challenge2/test_d1c2.py
+++ day01/challenge2/test_d1c2.py
@@ -7,7 +7,7 @@ from d1c2 import count_increases_sliding_window
 
 def test_count_increases_from_file():
     current_directory = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(current_directory, "input.txt")
+    file_path = os.path.join(current_directory, "../input.txt")
     with open(file_path, "r") as file:
         depths = [int(line.strip()) for line in file.readlines()]
     result = count_increases_sliding_window(depths) 
